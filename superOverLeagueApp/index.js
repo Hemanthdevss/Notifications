1c37e112718643f2e8aa005ec023dcfb20782ca5
Hemanthdevss <Hemanth.devss@gmail.com>
2023-11-23 09:21:43 +0000

    notification

---
+++ superOverLeagueApp/index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="bg_container">
+    <h1 className="headingColor"> Super Over League </h1>
+    <div className="imageContainer">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="imageSize"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="imageSize"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
